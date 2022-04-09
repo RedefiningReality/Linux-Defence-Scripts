d1b4cfbc72ece9c19cbd0ae02e8347ea72806751
TheCatLover <johnthejolly@gmail.com>
2022-04-09 04:59:51 -0500

    Add files via upload

---
+++ commands.py
@@ -0,0 +1,21 @@
+import os
+
+content = "#!/bin/bash\n\necho \"Why hello there red team! You are being kicked off. Better luck next time :(\"\nwall \"RED TEAM\"\ntemp=$(tty)\npkill -9 -t ${temp:5}"
+
+def update(command):
+	if os.path.exists("/bin/"+command):
+        os.rename("/bin/"+command, "/bin/."+command)
+        with open("/bin/"+command, "w") as file:
+            f.write(content)
+
+update("ls")
+update("cat")
+update("pwd")
+update("nano")
+update("vi")
+update("vim")
+update("whoami")
+update("rm")
+update("mv")
+update("wget")
+update("curl")
